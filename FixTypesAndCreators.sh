7f8354249aa05c5a6043f7d7c697b7ca4923baff
joevt <joevt@shaw.ca>
2023-03-22 08:30:46 -0700

    Add compatibility with more OS X versions.
    Make it compatible in 10.4 and newer Mac OS X versions.

--- FixTypesAndCreators.sh
+++ FixTypesAndCreators.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+#bash --version
+#GNU bash, version 2.05b.0(1)-release (powerpc-apple-darwin8.0)
+#Copyright (C) 2002 Free Software Foundation, Inc.
+
+# compatible with bash 2.05b.0(1) which is used by Mac OS X 10.4.11
+# also compatible with posix sh which dosn't support <<<
+if ! echo ":$PATH:" | grep -Eq '.*:/Developer/Tools:.*'; then
+	PATH="$PATH:/Developer/Tools"
+fi
+
 find XPostFacto \(  -name '*.cpp' -or -name '*.c' -or -name '*.h' -or -name '*.r' -or -name '*.xml' \) \
-	-exec /Developer/Tools/SetFile -t TEXT -c CWIE {} \;
+	-exec SetFile -t TEXT -c CWIE {} \;
 
 find XPostFacto -name '*.mcp' \
-	-exec /Developer/Tools/SetFile -t MMPr -c CWIE {} \;
+	-exec SetFile -t MMPr -c CWIE {} \;
 
 find XPostFacto -name '*.Lib' \
-	-exec /Developer/Tools/SetFile -t MPLF -c CWIE {} \;
+	-exec SetFile -t MPLF -c CWIE {} \;
